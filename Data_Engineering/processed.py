b97f4d8c5278d3a3161980b8c7f930958a65f1da
aditya <meenakshi.bhtt@gmail.com>
2023-10-03 08:24:27 +0530

    started to work on eda script

---
+++ Data_Engineering/processed.py
@@ -0,0 +1,76 @@
+'''
+Author - Aditya Bhatt 7:00 AM
+Objectives - 
+1.Write code to read all files from raw container and then dump the same to processed container after applying the following transformations-
+1.1 Remove Null's
+1.2 Extract Month from from_date
+1.3 Drop from_date and to_date
+1.4 Add a column for State (Not Sure will add this is this code to later)
+'''
+
+#Import necessary Modules
+from azure.storage.blob import BlobServiceClient
+import pandas as pd
+import time
+from io import BytesIO
+from azure.storage.blob import *
+
+
+#GLOBAL VARIABLES
+STORAGEACCOUNTURL= "https://dataairquality.blob.core.windows.net/"
+STORAGEACCOUNTKEY= "R0zWp7D/7qFb1y4nkNg1gFkJepOmjnb57SjDqv7KjhOej/DwJgRS/uLn1/r1tK08BrEN5/Z103L8+AStJFr8Eg=="
+CONTAINERNAME= "raw"
+
+ACCOUNT_NAME="dataairquality"
+ACCOUNT_KEY="R0zWp7D/7qFb1y4nkNg1gFkJepOmjnb57SjDqv7KjhOej/DwJgRS/uLn1/r1tK08BrEN5/Z103L8+AStJFr8Eg=="
+DEST_CONTAINER="processed"
+
+
+
+
+# Connect to Blob Storage
+blob_service_client = BlobServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+
+# Get a reference to the container
+container_client = blob_service_client.get_container_client(CONTAINERNAME)
+
+# List all blobs in the container
+blob_list = [blob.name for blob in container_client.list_blobs()]
+
+
+#Iterating through the list of blobs and applying the transformations and dumping the same in Processed Container
+for i in blob_list:
+    BLOBNAME=i
+    #download from blob
+    blob_service_client_instance = BlobServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    blob_client_instance = blob_service_client_instance.get_blob_client(CONTAINERNAME, BLOBNAME, snapshot=None)
+    blob_data = blob_client_instance.download_blob()
+    content = blob_data.readall()
+
+    #Convert data to a format that is understood by pandas
+    content_io = BytesIO(content)
+
+    # Perform transformations using pandas
+    df = pd.read_csv(content_io)  
+    df.drop(['BP (mmHg)'],axis=1,inplace=True)
+    df=df.dropna()
+    df.drop(['To Date'],axis=1,inplace=True)
+    df['From Date'] = pd.to_datetime(df['From Date'])
+    df['Month'] = df['From Date'].dt.month
+    df['filename']=BLOBNAME.split(".")[0]
+    df.drop(['From Date'],axis=1,inplace=True)
+    
+    
+    output = df.to_csv (encoding = "utf-8",index=False)
+
+
+    #Pushing the data back to processed Container
+    blob_service = BlobServiceClient.from_connection_string(
+        f"DefaultEndpointsProtocol=https;AccountName={ACCOUNT_NAME};AccountKey= {ACCOUNT_KEY};EndpointSuffix=core.windows.net"
+    )
+
+    container_client = blob_service.get_container_client(DEST_CONTAINER)
+    blob_client = blob_service.get_blob_client(container=DEST_CONTAINER, blob="t_"+f"{i}") 
+
+    blob_client.upload_blob(output,overwrite=True,content_settings=ContentSettings(content_type="text/csv"))
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Let-India-Breathe
+This project is a part of Environmental AI 
+# Work in Progress
